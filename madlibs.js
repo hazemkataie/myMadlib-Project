df131189b85fa2e57aefadc72b2aa12ef41fba04
Hazem Kataie <hazem.kataie@stu.fbu.edu.tr>
2023-06-18 13:21:29 +0300

    building the foundations

---
+++ madlibs.js
@@ -0,0 +1,10 @@
+// List of prompts for the user
+var prompts = [
+	'Type your name',
+	'Type an adjective',
+	'Type a noun'
+   ];
+//Array to keep the answer together.
+var answers=[];
+// Keep track of current prompt we're on
+var currentPrompt = 0;
