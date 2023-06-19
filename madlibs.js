42ef62cf8c594b2fddd850344c7c2b7b67b64a0e
Hazem Kataie <hazem.kataie@stu.fbu.edu.tr>
2023-06-19 14:05:15 +0300

    Add more JS

--- madlibs.js
+++ madlibs.js
@@ -4,7 +4,29 @@ var prompts = [
 	'Type an adjective',
 	'Type a noun'
    ];
-//Array to keep the answer together.
-var answers=[];
+
 // Keep track of current prompt we're on
 var currentPrompt = 0;
+
+// A function that will call the next prompt
+var nextPrompt = function() {
+	//if there is a next prompt
+	if(currentPrompt < prompts.length){
+	//put current prompt in all html elements with class 
+	  $('.prompt').html(prompts[currentPrompt]);
+	// move the next prompt into variable currentPrompt 
+	  currentPrompt = currentPrompt + 1;
+	}
+	//or else if we're at the end ot the array
+	else {
+	  $('.prompt').html("that's all for now!");
+	}
+}
+
+// run nextPrompt function when button is clicked
+$('button').click(function() {
+	nextPrompt();
+});
+
+//Show the first prompt as soon as js loads
+nextPrompt();
