b2b287bc0a398b64b5d322b6259305f821c6fa73
Hazem Kataie <hazem.kataie@stu.fbu.edu.tr>
2023-06-20 11:20:00 +0300

    Uptade codes

--- madlibs.js
+++ madlibs.js
@@ -4,38 +4,37 @@ var prompts = [
 	'Type an adjective',
 	'Type a noun'
    ];
-//here's an array to keep answers in
-var answers = [];
 
+var answers=[];
 // Keep track of current prompt we're on
 var currentPrompt = 0;
 
 // A function that will call the next prompt
 var nextPrompt = function() {
-  //there's no answer the first time nextPrompt
-  if(currentPrompt != 0){
-    answers.push($('input').val()); 
+  //if there's no answer in the form
+  if (currentPrompt != 0){
+    answers.push($('input').val());
   }
 	// if there is a next prompt
 	if (currentPrompt < prompts.length) {
-	  //put prompt and form into your html
-	  
 		// put first prompt in all html elements with class 
-		$('.prompt').html(prompts[currentPrompt] + '<br><input type="text">');
+		$('.prompt').html(prompts[currentPrompt] +'<br><input type="text">');
 		// move the next prompt into variable currentPrompt 
 		currentPrompt = currentPrompt + 1;
 	}
 	//or else if we're at the end of the array
 	else {
-		//run showFinal function
+		// put a new message into the html.
 		showFinal();
 	}
 }
-//puts the user's input into the html
-var showFinal = function() {
-  $('.prompt').html(answers[0]+' '+answers[1]+' '+answers[2]); 
-}
 
+//puts user answers into HTML
+var showFinal = function() {
+  $('.prompt').html('This is the story of <span class="fill">'+answers[0]+'</span> and the <span class="fill">'+answers[1]+'</span> <span class="fill">'+answers[2]+'</span>.');
+  //and then hide the button
+  $('button').hide();
+}
 // run nextPrompt function when button is clicked
 $('button').click(function() {
 	nextPrompt();
